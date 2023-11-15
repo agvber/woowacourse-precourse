f229a1c014d7a0efde8892243f1d715f5bd7ecc4
MinJun <agvber12@gmail.com>
2023-11-15 15:40:19 +0900

    feat: OutputView 이벤트 제목 출력 함수 추가

--- src/main/kotlin/christmas/ui/OutputView.kt
+++ src/main/kotlin/christmas/ui/OutputView.kt
@@ -9,4 +9,8 @@ class OutputView {
     fun printErrorLog(t: Throwable) {
         println(t.message ?: "[ERROR] 알 수 없는 에러가 발생 하였습니다.")
     }
+
+    fun printEventPreviewTitle(visitDay: Int) {
+        println("12월 ${visitDay}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n")
+    }
 }
