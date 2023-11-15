5dce4ece5933a6bd8943eb3372eb368e53cc1df1
MinJun <agvber12@gmail.com>
2023-11-15 15:17:36 +0900

    feat: Restaurant 주문 메뉴 변수 지정 및 데이터 값 체크

--- src/main/kotlin/christmas/domain/Restaurant.kt
+++ src/main/kotlin/christmas/domain/Restaurant.kt
@@ -1,5 +1,7 @@
 package christmas.domain
 
+import christmas.data.Courses
+import christmas.data.Menu
 import christmas.model.OrderMenu
 
 class Restaurant {
@@ -19,10 +21,61 @@ class Restaurant {
         }
     }
 
+    fun putOrderMenus(inputOrderMenus: String) {
+        try {
+            val splitList = inputOrderMenus.split(",")
+            orderMenus = splitList.toOrderMenu()
+            checkDuplicateOrderMenu(orderMenus)
+            checkMaximumOrderMenu(orderMenus)
+            checkOnlyDrink(orderMenus)
+        } catch (e: Exception) {
+            val errorMessage = e.message ?: "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."
+            throw IllegalArgumentException(errorMessage)
+        }
+    }
+
 }
 
 private fun checkVisitDay(day: Int) {
     if (day !in 1..31) {
         throw IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.")
+    }
+}
+
+private fun List<String>.toOrderMenu(): List<OrderMenu> {
+    val menus = Menu.values()
+    return this.map { stringList ->
+        val values = stringList.split("-")
+        OrderMenu(
+            name = menus.first { values[0] == it.name },
+            count = values[1].toInt().also {
+                if (it < 1) {
+                    throw IllegalArgumentException()
+                }
+            }
+        )
+    }
+}
+
+private fun checkDuplicateOrderMenu(orderMenus: List<OrderMenu>) {
+    val orderNames = orderMenus.map { it.name }
+    val orderNameSet = orderNames.toSet()
+
+    if (orderNames.size != orderNameSet.size) {
+        throw IllegalArgumentException()
+    }
+}
+
+private fun checkMaximumOrderMenu(orderMenus: List<OrderMenu>) {
+    if (orderMenus.sumOf { it.count } > 20) {
+        throw IllegalArgumentException()
+    }
+}
+
+private fun checkOnlyDrink(orderMenus: List<OrderMenu>) {
+    val findItem = orderMenus.find { it.name.courses != Courses.DRINK }
+
+    if (findItem == null) {
+        throw IllegalArgumentException("[ERROR] 음료만 주문할 수 없습니다. 다시 입력해 주세요.")
     }
 }
