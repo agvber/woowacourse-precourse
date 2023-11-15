28994393c30fd57cbeb9e865c8a78579c1020033
MinJun <agvber12@gmail.com>
2023-11-15 15:12:41 +0900

    feat: Restaurant 방문일 변수로 지정 및 체크 함수 추가

--- src/main/kotlin/christmas/domain/Restaurant.kt
+++ src/main/kotlin/christmas/domain/Restaurant.kt
@@ -10,4 +10,19 @@ class Restaurant {
     fun getVisitDay() = visitDay
     fun getOrderMenus() = orderMenus
 
+    fun putVisitDay(inputDay: String) {
+        try {
+            visitDay = inputDay.toInt()
+            checkVisitDay(visitDay)
+        } catch (e: Exception) {
+            throw IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.")
+        }
+    }
+
+}
+
+private fun checkVisitDay(day: Int) {
+    if (day !in 1..31) {
+        throw IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.")
+    }
 }
