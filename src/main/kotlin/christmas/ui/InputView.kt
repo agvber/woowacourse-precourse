39a94e06c687dc734fd6f0abb3cc717954c02129
MinJun <agvber12@gmail.com>
2023-11-15 15:34:25 +0900

    feat: InputView 사용자로 부터 날짜 받아오기

--- src/main/kotlin/christmas/ui/InputView.kt
+++ src/main/kotlin/christmas/ui/InputView.kt
@@ -1,5 +1,20 @@
 package christmas.ui
 
+import camp.nextstep.edu.missionutils.Console
+
 class InputView {
-    // TODO: 구현
+    fun readDate(orderCallback: (String) -> Boolean) {
+        println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
+        orderCallback(orderCallback)
+    }
+}
+
+private fun orderCallback(callback: (String) -> Boolean) {
+    while (true) {
+        val readLine = Console.readLine()
+        val isBreak = callback(readLine)
+        if (isBreak) {
+            break
+        }
+    }
 }
