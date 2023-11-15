071af735683e203b2130868e2d4c7903e81b3795
MinJun <agvber12@gmail.com>
2023-11-15 15:19:01 +0900

    test: Restaurant 주문 예외 처리 테스트

--- src/test/kotlin/christmas/RestaurantTest.kt
+++ src/test/kotlin/christmas/RestaurantTest.kt
@@ -1,6 +1,8 @@
 package christmas
 
+import christmas.data.Badge
 import christmas.domain.Restaurant
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -20,4 +22,30 @@ class RestaurantTest {
             restaurant.putVisitDay("34")
         }
     }
+
+    @Test
+    fun `주문 예외 테스트`() {
+        assertThrows<IllegalArgumentException> {
+            restaurant.putOrderMenus("해산물파스타-2,레드와인-1,초코케이크-a")
+        }
+    }
+
+    @Test
+    fun `음료만 주문할 경우에 예외처리 테스트`() {
+        assertThrows<IllegalArgumentException>(
+            message = "[ERROR] 음료만 주문할 수 없습니다. 다시 입력해 주세요."
+        ) {
+            restaurant.putOrderMenus("제로콜라-2,레드와인-1,샴페인-1")
+        }
+    }
+
+    @Test
+    fun `주문 항목이 20개가 넘을 경우 예외 처리 테스트`() {
+        assertThrows<IllegalArgumentException>(
+            message = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."
+        ) {
+            restaurant.putOrderMenus("해산물파스타-20,레드와인-1")
+        }
+    }
+
 }
